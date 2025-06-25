a3973de97ae1ba7b7a92d1dd42991c884ea7f99b
RakeshKumarPagadala <p.rakesh801ra@gmail.com>
2025-06-25 20:02:18 +0530

    commit-3

--- app/(main)/dashboard/create-interview/page.jsx
+++ app/(main)/dashboard/create-interview/page.jsx
@@ -13,7 +13,7 @@ import InterviewLink from './_components/InterviewLink'
 function CreateInterview() {
     const router = useRouter()
     const [step, setStep] = useState(1)
-    const [formData, setFormData] = useState()
+    const [formData, setFormData] = useState({})
     const [interviewId, setInterviewId] = useState()
 
     const onHandleInputChange=(field,value) => {
